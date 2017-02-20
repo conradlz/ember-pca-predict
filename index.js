67170ea8286d1e45d42331df7d891cb5ed06ea90
Prince_Corn <konradlippertzajaczkowski@gmail.com>
2017-02-20 15:23:29 -0600

    FIX: fastboot compatibility

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 /* eslint-env node */
 'use strict';
 
+var filterInitializers = require('fastboot-filter-initializers');
+
 module.exports = {
   name: 'ember-pca-predict',
+  preconcatTree: function(tree) {
+    return filterInitializers(tree, this.app.name);
+  },
   isDevelopingAddon() {
     return true;
   }
