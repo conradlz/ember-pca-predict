85b1f9e9f8919d5a6365ef61761beec10dddfdca
Conrad Lippert-Zajaczkowski <lippert-mac@Conrads-Mac-mini.local>
2021-04-17 20:26:47 -0500

    HTTPS required

--- app/instance-initializers/pca-setup.js
+++ app/instance-initializers/pca-setup.js
@@ -19,7 +19,7 @@ export function initialize(appInstance) {
         }, n[i].on = n[i].on || function() {
             (n[i].onq = n[i].onq || []).push(arguments)
         }, u = t.createElement("script"), u.async = !0, u.src = r, f = t.getElementsByTagName("script")[0], f.parentNode.insertBefore(u, f)
-    })(window, document, "pca", `//${emberPcaConfig.host}/js/sensor.js`)
+    })(window, document, "pca", `https://${emberPcaConfig.host}/js/sensor.js`)
   }
 }
 
